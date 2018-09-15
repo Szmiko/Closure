d1f333652c2ce8e9f32b136669265af08442c585
Szymon Grabowski <szmiko@wp.pl>
2018-09-15 14:57:15 +0200

    New End

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
-function makeCounter() {
-	var count = 0;
+//without closure:
 
-	function counter() {
-		count = count + 1;
-		return count;
-	}
-	return counter;
+/*var count = 0;
+
+window.onload = function() {
+	var button = document.getElementById("clickme");
+	button.onclick = handleClick;
 };
 
-var doCount = makeCounter();
-console.log(doCount());
+function handleClick() {
+	var message = "Kliknąłeś mnie ";
+	var div = document.getElementById("message");
+	count++;
+	div.innerHTML = message + count + " razy!";
+};*/
+
+//with closure:
+
+window.onload = function() {
+	var count = 0;
+	var message = "Kliknąłeś mnie ";
+	var div = document.getElementById("message");
+	var button = document.getElementById("clickme");
+
+	button.onclick = function() {
+		count++;
+		div.innerHTML = message + count + " razy!";
+	}
+};
+
+
+
+
+
+
+
+
+
+
+// ------------------------------
+/*function makeCounter() {
+	var count = 0;
+
+	return {
+		increment: function() {
+			count++;
+			return count;
+		}
+	}
+};
+
+var counter = makeCounter();
+console.log(counter.increment());
+*/
